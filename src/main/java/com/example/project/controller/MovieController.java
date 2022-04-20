316ae887275417fdd6b86d959a27df80fd668fb9
nisaggarwal1 <nisaggarwal@deloitte.com>
2022-04-20 18:17:56 +0530

    initial commit

--- src/main/java/com/example/project/controller/MovieController.java
+++ src/main/java/com/example/project/controller/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,9 @@ import com.example.project.service.MovieService;
 @RequestMapping("/movie")
 public class MovieController {
 	final Logger logger = LoggerFactory.getLogger(getClass());
-	String tokenValue = "Secret";
+	@Value("${authorization.tokenValue}")
+    String tokenValue;
+	@Value("${headerKey.timeTaken}")
 	String headerKey = "X-TIME-TO-EXECUTE";
 	@Autowired
 	MovieService movieService;
